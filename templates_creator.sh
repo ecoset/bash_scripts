c3ec4e1e8769e18a17eccc3de829f6d587fb9e5e
ecoset <srg.or@proton.me>
2026-02-23 21:35:05 +0300

    Created , added arrays of directories and files.

---
+++ templates_creator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir_templates="$HOME/Templates/"
+
+folders=("prog" "text" "web")
+
+files_prog=("assembly.asm" "c++.cpp" "golang.go" "nim.nim" "perlModule.pm" "ruby.rb" "bash-sh.sh" "c#.cs" "header.h" "ObjC.m" "perl.pl" "rust.rs" "c.c" "falcon.fal" "java.java" "perl6.pl" "python3.py" "shellcode.s")
+files_text=("document.odt" "plaintext.txt" "presentation.odp" "spreadsheet.ods")
+files_web=("css.css" "html.html" "javascript.js" "php.php" "xml.xml")
